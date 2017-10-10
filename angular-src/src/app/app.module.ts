62c7f5b0b0fc85bdb817577fc26720ee692b9fad
Daniel Cook <dmc3152@yahoo.com>
2017-10-10 10:39:47 -0700

    moved header and sidebar into components

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 import { AppComponent } from './app.component';
 
@@ -12,9 +14,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    SharedModule,
+    CoreModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- angular-src/src/app/core/core.module.ts
+++ angular-src/src/app/core/core.module.ts
@@ -3,15 +3,24 @@ import { SharedModule } from '../shared/shared.module';
 import { CoreRoutingModule } from './core-routing.module';
 
 import { AuthService } from './auth.service';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [
     SharedModule,
     CoreRoutingModule,
   ],
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    SidebarComponent
+  ],
   providers: [
     AuthService
+  ],
+  exports: [
+    HeaderComponent,
+    SidebarComponent
   ]
 })
 export class CoreModule { }
