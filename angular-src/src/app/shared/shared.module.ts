4ffd4914a8ad4db0a81228db5be95356e828a889
Daniel Cook <dmc3152@yahoo.com>
2017-11-03 11:43:07 -0700

    added password directive shell

--- angular-src/src/app/shared/shared.module.ts
+++ angular-src/src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PasswordDirective } from './password.directive';
 
 @NgModule({
   imports: [
@@ -8,7 +9,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     FormsModule,
     ReactiveFormsModule,
   ],
-  declarations: [],
+  declarations: [PasswordDirective],
   exports: [
     CommonModule,
     FormsModule,
